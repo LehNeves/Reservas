7a61af3acc8d200010a659844f64889100cb7437
Melqui <melki2011@gmail.com>
2019-05-23 01:39:52 -0300

    Listando Categorias na Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,26 +2,26 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/reservas.dto';
 
 @IonicPage()
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
+  items: CategoriaDTO[];
   constructor(
     public navCtrl: NavController,
-    public categoriaService: CategoriaService
-    ) {
-    
-  }
+    public categoriaService: CategoriaService) {}
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response =>{
-      console.log(response);
+    this.categoriaService.findAll()
+    .subscribe(response =>{
+      this.items = response;
     },
     error => {
       console.log(error);
