07269cb120fba7dc578f14393db10ab6bd3c06a6
LehNeves <lneves997@gmail.com>
2019-06-09 03:10:29 -0300

    Atualização da página home e adição de imagens

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,24 +3,45 @@ import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categorias.dto';
+import { ViewChild } from '@angular/core';
+import { RestauranteService } from '../../services/domain/restaurante.service';
+import { RestauranteDTO } from '../../models/restaurante.dto';
 
 @IonicPage()
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
+  
 })
 export class HomePage {
+  @ViewChild('mySlider') mySlider: any;
+
+  slideChanged() {
+    this.mySlider.slideTo(1, 2000);
+  }
+
+  goToSlide() {
+    this.mySlider.slideTo(2, 500);
+  }
   items: CategoriaDTO[];
+  rest: RestauranteDTO[];
 
   constructor(
     public navCtrl: NavController,
-    public categoriaService: CategoriaService) {}
+    public categoriaService: CategoriaService,
+    public restauranteService: RestauranteService) {}
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
     .subscribe(response =>{
       this.items = response;
+    },
+    error => {});
+    
+    this.restauranteService.findAll()
+    .subscribe(response =>{
+      this.rest = response;
     },
     error => {});
   }
